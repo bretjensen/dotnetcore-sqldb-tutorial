27c3de8973ed241692919b15f5e257a5b1af58d4
Brett Jensen <bretjensen@gmail.com>
2019-03-11 20:18:13 +0000

    Change connection to development and connect to sql azure

--- Startup.cs
+++ Startup.cs
@@ -29,12 +29,12 @@ namespace DotNetCoreSqlDb
             services.AddMvc();
 
         // Use SQL Database if in Azure, otherwise, use SQLite
-        if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+        // if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
             services.AddDbContext<MyDatabaseContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-        else
-            services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+        // else
+        //     services.AddDbContext<MyDatabaseContext>(options =>
+        //             options.UseSqlite("Data Source=localdatabase.db"));
 
         // Automatically perform database migration
         services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();        }
